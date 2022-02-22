1aa7e689cf12289d038d814972845618e5672408
Tian-Rong <93510797+Tian-Rong@users.noreply.github.com>
2022-02-22 22:29:44 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,58 +1,41 @@
-import socket
+import unittest
+
 import requests
 import scrapy
 
-HOST, PORT = "", 8080
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-s.bind((HOST, PORT))
-s.listen(True)
-
-print("Serving HTTP on port %s..."%PORT)
-
-while True:
-    client_connection, client_address = s.accept()
-    request = client_connection.recv(1024)
-    print(request.decode("utf-8"))
-    http_response = """\
-HTTP/1.1 200 OK
-
-Welcome This is my first webpage. Great!
-"""
-
-    client_connection.sendall(bytes(http_response, "utf-8"))
-    client_connection.close()
-
-url= 'http://www.wikipedia.org'
+url= 'https://brickset.com/sets/year-2010'
 r= requests.get(url)
-
-print(r.text)
-
-print("Status code:")
-print("\t*", r.status_code)
+print("status code")
+print("\t *", r.status_code, "OK")
 
 h=requests.head(url)
-print("Header.")
+print(url)
+print("Header:")
 print("**********")
 
 for x in h.headers:
     print("\t",x,".",h.headers[x])
-
 print("**********")
 
+
+class Testcase(unittest.TestCase):
+    def testExample(self):
+        print("testing")
+
 headers = {
-    'User-Agent' : 'Iphone 8'
+    'User-Agent' : 'Mobile'
 }
-url2 = "http://httpbin.org/headers"
+
+url2 = 'http://httpbin.org/headers'
 rh = requests.get(url2, headers=headers)
 print(rh.text)
 
 class NewSpider(scrapy.Spider):
     name = "new_spider"
-    start_urls = ["http://brickset.com/sets/year-2019"]
+    start_urls = ["http://brickset.com/sets/year-2010"]
     def parse(self, response):
-        xpath_selector = '//img'
+        xpath_selector='//img'
         for x in response.xpath(xpath_selector):
             newsel = '@src'
             yield {
@@ -66,3 +49,5 @@ class NewSpider(scrapy.Spider):
                 response.urljoin(next_page),
                 callback=self.parse
             )
+if __name__ == '__main__':
+    unittest.main()
